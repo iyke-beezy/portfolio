73c41ef1fc999d0f3c9eeb2305d8af84725b4a6e
= <=>
2022-08-02 14:00:27 +0000

    Made changes to header Information

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
 					</div>
 
 					<div className="tag-cmp app__flex">
-						<p className="p-text">Web Developer</p>
+						<p className="p-text">Full-Stack Web Developer</p>
 						<p className="p-text">Freelancer</p>
 					</div>
 				</div>
@@ -60,7 +60,7 @@ const Header = () => {
 				whileInView={scaleVariants.whileInView}
 				className="app__header-circles"
 			>
-				{[images.flutter, images.redux, images.sass].map((circle, index) => (
+				{[images.node, images.redux, images.sass].map((circle, index) => (
 					<div className="circe-cmp app__flex" key={`circle-${index}`}>
 						<img src={circle} alt="" circle />
 					</div>
